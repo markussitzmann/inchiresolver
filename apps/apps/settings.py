7bf92c4626cda36e2424941a8d69f2f2650ac0f1
Markus Sitzmann <markus.sitzmann@gmail.com>
2016-08-17 00:55:18 +0200

    clean up of media and static, new location of these dirs in /home/service, gitignore cleanup

--- apps/apps/settings.py
+++ apps/apps/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join("/home/service", "static/")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = os.path.join("/home/service", "media/")
 MEDIA_URL = "/media/"
 
 
